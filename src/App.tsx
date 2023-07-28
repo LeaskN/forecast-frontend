6ce2c87661c27ea73692165696a365bf2c609bc9
Nicholas Leask <leask.nicholas@gmail.com>
2023-07-28 12:46:14 -0400

    feat: converted app.js to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,30 @@ import React, { useEffect, useState } from 'react';
 import UserLocationInput from './Components/UserLocationInput/UserLocationInput';
 import PeriodWeather from './Components/PeriodWeather/PeriodWeather';
 
+interface UserAddress {
+  street: string;
+  city: string;
+  state: string;
+  zipcode: string;
+}
+
+interface UserLatLon{
+  lat: string;
+  lng: string;
+}
+
 function App() {
-  const [userLocation, setUserLocation] = useState(null);
-  const [userAddress, setUserAddress] = useState({});
-  const [userLatLon, setUserLatLon] = useState({});
+  const [userUpcomingForecast, setUserUpcomingForecast] = useState(null);
+  const [userAddress, setUserAddress] = useState<UserAddress>({ street: '', city: '', state: '',   zipcode: ''});
+  const [userLatLon, setUserLatLon] = useState<UserLatLon>({lat: '', lng: ''});
   const [upcomingForcast, setUpcomingForcast] = useState([])
 
   useEffect(() => {
-    const successCallback = (position) => {
-      setUserLocation(position);
+    const successCallback = (position: any) => {
+      setUserUpcomingForecast(position);
     };
 
-    const errorCallback = (error) => {
+    const errorCallback = (error: any) => {
       console.log(error);
     };
 
@@ -23,7 +35,7 @@ function App() {
 
   useEffect(() => {
     getAddressFromLatLng();
-  }, [userLocation]);
+  }, [userUpcomingForecast]);
 
 
   useEffect(() => {
@@ -42,10 +54,10 @@ function App() {
 
 
   const getLatLngFromAddress = async () => {
-    if (userAddress.street && userAddress.city && userAddress.state) {
-      const street = userAddress.street.split(' ').join('+');
-      const city = userAddress.city.split(' ').join('+');
-      const state = userAddress.state.split(' ').join('+');
+    if (userAddress?.street && userAddress?.city && userAddress?.state) {
+      const street = userAddress?.street.split(' ').join('+');
+      const city = userAddress?.city.split(' ').join('+');
+      const state = userAddress?.state.split(' ').join('+');
 
       await fetch(`https://weather-backend-1dcb6a57dec1.herokuapp.com/latlon/?street=${street}&city=${city}&state=${state}&benchmark=Public_AR_Census2020&vintage=Census2020_Census2020&layers=10&format=json`)
         .then(res => res.json())
@@ -58,10 +70,10 @@ function App() {
   }
   
   const getWeather = async () => {
-    if (userAddress.street && userAddress.city && userAddress.state) {
+    if (userAddress?.street && userAddress?.city && userAddress?.state) {
       await fetch(`https://weather-backend-1dcb6a57dec1.herokuapp.com/weather/?lat=${userLatLon.lat}&lng=${userLatLon.lng}`)
         .then(res => res.json())
-        .then(res => setUpcomingForcast(res.properties.periods));
+        .then(res => setUpcomingForcast(res?.properties?.periods));
     }
   }
   
@@ -71,16 +83,14 @@ function App() {
 
   return (
     <div className="App">
-      {userLocation === null ?
         <UserLocationInput
-          street={userLocation?.street}
-          city={userLocation?.city}
-          state={userLocation?.state}
-          zipcode={userLocation?.zipcode}
-          country={userLocation?.country}
-          onClick={(newAddress) => setUserAddress(newAddress)}
-        /> :
-        <CheckUserAddress />}
+          street=''
+          city=''
+          state=''
+          zipcode=''
+          onClick={(userAddress:UserAddress) => setUserAddress(userAddress)}
+        /> 
+         {/* :<CheckUserAddress /> */}
       {userLatLon?.lng ? `lat: ${userLatLon.lat}lng: ${userLatLon.lng}` : ''}
       <div className='periodWeatherContainer' >
       {upcomingForcast.length > 0 ? 
@@ -91,13 +101,13 @@ function App() {
   );
 }
 
-function CheckUserAddress({ checkAddress, address }) {
-  return (
-    <>
-      <p>If address is inaccurate, click here</p>
-      <button onClick={checkAddress}>Wrong Address</button>
-    </>
-  )
-}
+// function CheckUserAddress({ checkAddress, address }) {
+//   return (
+//     <>
+//       <p>If address is inaccurate, click here</p>
+//       <button onClick={checkAddress}>Wrong Address</button>
+//     </>
+//   )
+// }
 
 export default App;
