006c8decd30eefa2a247ec11602d9d4b995701d1
Nicholas Leask <leask.nicholas@gmail.com>
2023-07-28 14:28:08 -0400

    feat: formatting

--- src/Components/PeriodWeather/PeriodWeather.tsx
+++ src/Components/PeriodWeather/PeriodWeather.tsx
@@ -28,12 +28,12 @@ interface Props{
 
 const PeriodWeather = ( { period } : Props ) => {
   return (<div className={`periodWeatherCard ${period?.isDaytime ? 'daytime' : 'nightTime'}`}    >
-    <img style={{ borderRadius: '5px', border: '3px solid' }} src={period?.icon} alt='periodIcon' />
     <h1>{period?.name}</h1>
+    <img style={{ borderRadius: '5px', border: '3px solid' }} src={period?.icon} alt='periodIcon' />
+    <p>{period?.shortForecast}</p>
     <p>{period?.temperature}{period?.temperatureUnit}&deg;</p>
     <p>Wind: {period?.windSpeed} {period?.windDirection}</p>
     <p>Dewpoint: {Math.round(period?.dewpoint?.value)}&deg; {period?.dewpoint?.unitCode.slice(-1)}</p>
-    <p>{period?.shortForecast}</p>
     <p>{period?.probabilityOfPrecipitation?.value}% Chance of Precipitation</p>
     <p>{period?.relativeHumidity?.value}% Humidity</p>
     <p>{period?.detailedForecast}</p>
