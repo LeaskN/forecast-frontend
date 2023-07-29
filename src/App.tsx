2da7b79ca87a8d822c78c5b68d4005f250e308eb
Nicholas Leask <leask.nicholas@gmail.com>
2023-07-28 22:57:27 -0400

    feat: added night mode

--- src/App.tsx
+++ src/App.tsx
@@ -12,32 +12,45 @@ interface UserAddress {
   zipcode: string;
 }
 
-interface UserLatLng{
+interface UserLatLng {
   lat: string;
   lng: string;
 }
 
 function App() {
-  const [userAddress, setUserAddress] = useState<UserAddress>({ street: '', city: '', state: '',   zipcode: ''});
-  const [userLatLng, setUserLatLng] = useState<UserLatLng>({lat: '', lng: ''});
+  const [userAddress, setUserAddress] = useState<UserAddress>({ street: '', city: '', state: '', zipcode: '' });
+  const [userLatLng, setUserLatLng] = useState<UserLatLng>({ lat: '', lng: '' });
   const [spinner, setSpinner] = useState<boolean>(false)
   const [upcomingForecast, setUpcomingForecast] = useState([]);
-  const [userCityFromBrowser, setUserCityFromBrowser] = useState({locality:''})
+  const [userCityFromBrowser, setUserCityFromBrowser] = useState({ locality: '' });
+
+  let night = false;
+
+  function isNight(){
+    const hours = new Date().getHours()
+    const isDayTime = hours > 6 && hours < 20;
+    if(isDayTime === true){
+      night = true;
+    }
+  }
 
   useEffect(() => {
-    const successCallback = async(geoLocationPosition: any) => {
+    const successCallback = async (geoLocationPosition: any) => {
       setUserLatLng({
         lat: geoLocationPosition.coords.latitude,
         lng: geoLocationPosition.coords.longitude
       })
       getUserCity();
     };
-    
+
     const errorCallback = (error: any) => {
       console.log(error);
     };
-    
+
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+    isNight();
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -45,12 +58,12 @@ function App() {
     getWeather();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userLatLng]);
-  
+
   useEffect(() => {
     getLatLngFromAddress();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userAddress]);
-  
+
   const getUserCity = async () => {
     await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${userLatLng.lat}&longitude=${userLatLng.lat}&localityLanguage=en`)
       .then(res => res.json())
@@ -61,18 +74,19 @@ function App() {
       const street = userAddress?.street.split(' ').join('+');
       const city = userAddress?.city.split(' ').join('+');
       const state = userAddress?.state.split(' ').join('+');
-      
+
       setSpinner(true);
       await fetch(`https://weather-backend-1dcb6a57dec1.herokuapp.com/latlon/?street=${street}&city=${city}&state=${state}&benchmark=Public_AR_Census2020&vintage=Census2020_Census2020&layers=10&format=json`)
         .then(res => res.json())
-        .then(res =>{
+        .then(res => {
           setUserLatLng({
             lat: res.addressMatches[0].coordinates.y,
             lng: res.addressMatches[0].coordinates.x
-        })});
+          })
+        });
     }
   }
-  
+
   const getWeather = async () => {
     if ((userAddress?.street && userAddress?.city && userAddress?.state) || (userLatLng.lat && userLatLng.lng)) {
       setSpinner(true);
@@ -86,10 +100,19 @@ function App() {
   return (
     <div className="App">
       {spinner ? <Spinner /> : ''}
+      <div className={!night ? `nightWrap` : ''}></div>
       <Clouds />
-      <UserLocationInput street='' city='' state='' zipcode='' onClick={(userAddress:UserAddress) => {setUserAddress(userAddress)}}/> 
-      {userCityFromBrowser?.locality && userAddress?.city === '' ? userCityFromBrowser.locality : ''}
-      {userAddress?.city?.length > 0 ? userAddress.city : ''}
+      <UserLocationInput street='' city='' state='' zipcode='' weatherDisplayed={upcomingForecast.length > 0 ? false : true} onClick={(userAddress: UserAddress) => { setUserAddress(userAddress) }} />
+      {userCityFromBrowser?.locality && userAddress?.city === '' ?
+        <div className='periodWeatherTopper'>
+          7-Day Forecast in {userCityFromBrowser.locality}
+        </div>
+        : ''}
+      {userAddress?.city?.length > 0 ?
+        <div className='periodWeatherTopper'>
+          7-Day Forecast in {userAddress.city}
+        </div>
+        : ''}
       <PeriodWeatherContainer spinner={spinner} upcomingForecast={upcomingForecast} />
     </div>
   );
