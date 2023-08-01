dc282ff1246f005b1e54c631d847f5679d448f84
Nicholas Leask <leask.nicholas@gmail.com>
2023-08-01 11:18:41 -0400

    fix: rename: spinner => loader

---
+++ src/Components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loader.css';
+
+function Loader() {
+  return(
+    <span className='spinnerContainer'>
+      <div id="loading"></div>
+    </span>
+  )
+}
+
+export default Loader;
